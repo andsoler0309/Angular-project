700ba6862c5a8325e60004fc4773541bdd6f5c84
Lara <the.lemonetti@gmail.com>
2022-11-19 18:19:48 -0800

    Se añaden pruebas del componente

--- src/app/exhibition/exhibition-create/exhibition-create.component.spec.ts
+++ src/app/exhibition/exhibition-create/exhibition-create.component.spec.ts
@@ -10,6 +10,7 @@ import { ReactiveFormsModule } from '@angular/forms';
 describe('ExhibitionCreateComponent', () => {
   let component: ExhibitionCreateComponent;
   let fixture: ComponentFixture<ExhibitionCreateComponent>;
+  let debug: DebugElement;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
@@ -23,9 +24,35 @@ describe('ExhibitionCreateComponent', () => {
     fixture = TestBed.createComponent(ExhibitionCreateComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
+    debug = fixture.debugElement;
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should have 1 <h1> element', () => {
+    expect(debug.queryAll(By.css('h1'))).toHaveSize(1);
+  });
+
+  it('should have 1 <form> element', () => {
+    expect(debug.queryAll(By.css('form'))).toHaveSize(1);
+  });
+
+  it('should have 3 <label> elements', () => {
+    expect(debug.queryAll(By.css('label'))).toHaveSize(3);
+  });
+
+  it('should have 2 <button> elements', () => {
+    expect(debug.queryAll(By.css('button'))).toHaveSize(2);
+  });
+
+  it('should have 2 <input> elements', () => {
+    expect(debug.queryAll(By.css('input'))).toHaveSize(2);
+  });
+
+  it('should have 1 <select> element', () => {
+    expect(debug.queryAll(By.css('select'))).toHaveSize(1);
+  });
+
 });
