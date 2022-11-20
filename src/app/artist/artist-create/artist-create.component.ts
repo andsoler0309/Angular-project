abf012fa4997eaf64849dffbcffe54c4c26a278d
Lara <the.lemonetti@gmail.com>
2022-11-20 13:09:24 -0800

    Se añade la primera versión de la lógica del componente

--- src/app/artist/artist-create/artist-create.component.ts
+++ src/app/artist/artist-create/artist-create.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from "@angular/forms";
+import { ToastrService } from 'ngx-toastr';
+import { ArtistService } from '../artist.service';
+import { Artist } from '../artist';
+
 
 @Component({
   selector: 'app-artist-create',
@@ -6,10 +11,34 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./artist-create.component.css']
 })
 export class ArtistCreateComponent implements OnInit {
+  artistForm!: FormGroup;
 
-  constructor() { }
+  constructor(private artistServive: ArtistService,
+    private formBuilder: FormBuilder,
+    private toastr: ToastrService) { }
 
   ngOnInit() {
+    this.artistForm = this.formBuilder.group({
+      name: ["", [Validators.required, Validators.minLength(2)]],
+      birthplace: ["", Validators.required],
+      birthdate: ["", Validators.required],
+      relatedMovements: ["", Validators.required,],
+    });
+  }
+
+  createExhibition(artist: Artist){
+    console.log(artist);
+//    this.exhibitionService.createExhibition(exhibition).subscribe(exhibition=>{
+//      console.info("The exhibition was created: ", exhibition)
+//      this.toastr.success("Confirmation", "Exhibition created")
+//      this.exhibitionForm.reset();
+//    })
+  }
+
+  cancelCreation(){
+    this.artistForm.reset();
+    console.info("The creation was canceled")
+    this.toastr.info("Confirmation", "Creation canceled")
   }
 
 }
